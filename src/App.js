691808a2d5a2e4c22050883bea1590ab03e5e9db
J00n4 <joona.ylijoki@gmail.com>
2021-11-17 20:05:37 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 import './App.css';
 import MyContainer from "./components/MyContainer";
-import {useState} from "react";
 
 function App() {
-  const [header, setHeader] = useState("Really epic list component");
-
-  const [items, setItems] = useState([
-      {id: "1", text: "This is an item"},
-      {id: "2", text: "Also this"}
-  ]);
 
   return (
     <div className="App">
       <h1>Hello World!</h1>
-      <MyContainer header={header} items={items} />
+      <MyContainer />
     </div>
   );
 }
--- src/components/MyContainer.js
+++ src/components/MyContainer.js
@@ -1,14 +1,19 @@
 import MyList from './MyList';
+import {useState} from "react";
 
 const List = (params) => {
     console.log("This is from MyContainer:" + params.items);
     console.log(params.items);
-    const head = params.header;
-    const list = params.items;
+    const [header, setHeader] = useState("Really epic list component");
+
+    const [items, setItems] = useState([
+        {id: "1", text: "This is an item"},
+        {id: "2", text: "Also this"}
+    ]);
     return(
         <div>
-            <h1>{head}</h1>
-            <MyList item={list} />
+            <h1>{header}</h1>
+            <MyList item={items} />
         </div>
     )
 }
