6a02610b4031be38216e49bf41afc7785be22b3a
J00n4 <joona.ylijoki@gmail.com>
2021-11-17 19:52:35 +0200

    update

--- src/components/MyContainer.js
+++ src/components/MyContainer.js
@@ -3,10 +3,13 @@ import MyList from './MyList';
 const List = (params) => {
     console.log("This is from MyContainer:" + params.items);
     console.log(params.items);
-    //const head = params.header;
-    //const list = params.items;
+    const head = params.header;
+    const list = params.items;
     return(
-        <MyList item={params} />
+        <div>
+            <h1>{head}</h1>
+            <MyList item={list} />
+        </div>
     )
 }
 
--- src/components/MyList.js
+++ src/components/MyList.js
@@ -1,25 +1,13 @@
-import {useState} from "react";
-
 function MyList(info) {
     console.log("This is 'info' from MyList:" + info.item);
-    console.log(info.item.header);
-    console.log(info.item.items);
+    console.log(info.item);
     console.log(info);
-    /*const [items, setItems] = useState([
-        {id: "1", text: "This is an item"},
-        {id: "2", text: "Also this"}
-    ]);*/
-    const head = info.item.header;
-    const items = info.item.items;
     return(
-        <div>
-            <h1>{head}</h1>
-            <ol>
-                {items.map((item) => (
-                    <li key={item.id}>{item.text}</li>
-                ))}
-            </ol>
-        </div>
+        <ol>
+            {info.item.map((item) => (
+                <li key={item.id}>{item.text}</li>
+            ))}
+        </ol>
     );
 }
 
