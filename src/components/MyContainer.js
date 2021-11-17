d6accb394654a93b3f1085650c4c160889fb8435
J00n4 <joona.ylijoki@gmail.com>
2021-11-17 21:37:29 +0200

    update

--- src/components/MyContainer.js
+++ src/components/MyContainer.js
@@ -8,8 +8,19 @@ function List() {
         {id: "1", text: "This is an item"},
         {id: "2", text: "Also this"}
     ]);
+    console.log(items.length);
+    const AddItem = () => {
+        const addArea = document.getElementById("textarea");
+        const newItem = {id: (items.length+1).toString(), text: document.getElementById("textarea").value};
+        console.log(newItem);
+        setItems([...items, newItem]);
+    }
     return(
-        <MyList header={header} items={items} />
+        <div>
+            <MyList header={header} items={items} />
+            <textarea id="textarea"></textarea>
+            <button onClick={AddItem}>Add item</button>
+        </div>
     )
 }
 
