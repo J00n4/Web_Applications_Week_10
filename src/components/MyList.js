4b8f5e14127447c9d1022cc09da892575dda86f1
J00n4 <joona.ylijoki@gmail.com>
2021-11-17 19:59:02 +0200

    update

--- src/components/MyList.js
+++ src/components/MyList.js
@@ -2,9 +2,10 @@ function MyList(info) {
     console.log("This is 'info' from MyList:" + info.item);
     console.log(info.item);
     console.log(info);
+    const information = info.item;
     return(
         <ol>
-            {info.item.map((item) => (
+            {information.map((item) => (
                 <li key={item.id}>{item.text}</li>
             ))}
         </ol>
