d54cc5c2f5355e4a0175b65184a8b7484b8dff46
J00n4 <joona.ylijoki@gmail.com>
2021-11-22 00:14:25 +0200

    update

---
+++ src/components/MyHOC.js
@@ -0,0 +1,14 @@
+const wrappedWithName = (OriginalComponent) => {
+    const NewComponent = (props) => {
+
+
+        return(
+            <div class="wrapper">
+                <OriginalComponent name={...props} />
+            </div>
+        )
+    }
+    return NewComponent;
+}
+
+export default wrappedWithName;
