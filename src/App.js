ca48e1841c037988419e6620bc99f0ae472cf335
J00n4 <joona.ylijoki@gmail.com>
2021-11-21 23:25:58 +0200

    update

--- src/App.js
+++ src/App.js
@@ -4,14 +4,20 @@ import About from "./components/About";
 import Header from "./components/Header";
 import Menu from "./components/Menu";
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
 
 function App() {
+  const { t, i18n } = useTranslation();
+
+  const changeLanguage = (lang) => {
+      i18n.changeLanguage(lang);
+  }
 
   return (
     <Router>
       <div className="App">
         <Routes>
-          <Route path="/" element={ <> <Header /> <MyContainer /> </> } />
+          <Route path="/" element={ <> <Header /> {t("This is the front page")} <MyContainer /> </> } />
           <Route path="/about" element={ <> <Header /> <About /> </> } />
         </Routes>
       </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,25 +2,44 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Appbar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
+//import Button from '@mui/material/Button';
 import MyContainer from './MyContainer';
 import Menu from "./Menu";
 import {Link} from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
+import React, {Suspense} from 'react';
 
 function Header() {
+    const { t, i18n } = useTranslation();
+
+    const changeLanguage = (lang) => {
+        i18n.changeLanguage(lang);
+    }
+
     return(
         <div>
             <Appbar position="static">
                 <Toolbar>
                     <IconButton edge="start" color="inherit" aria-label="home" sx={{ mr: 2 }}>
-                        <Link to="/">Home</Link>
+                        <Link to="/">{t("Home")}</Link>
                     </IconButton>
                     <IconButton edge="start" color="inherit" aria-label="about" sx={{ mr: 2 }}>
-                        <Link to="/about">About</Link> 
+                        <Link to="/about">{t("About")}</Link> 
                     </IconButton>
+                    <IconButton sx={{marginLeft: "auto"}} onClick={() => changeLanguage("fi")}>FI</IconButton>
+                    <IconButton onClick={() => changeLanguage("en")}>EN</IconButton>
                 </Toolbar>
             </Appbar>
         </div>
     )
 }
 
-export default Header;
+//export default Header;
+
+export default function App() {
+    return(
+        <Suspense fallback="loading">
+            <Header />
+        </Suspense>
+    )
+}
