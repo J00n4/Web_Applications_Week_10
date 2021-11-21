b404733cc6436fdcbd95ec5b52f7ffa8a0818eaa
J00n4 <joona.ylijoki@gmail.com>
2021-11-21 23:51:36 +0200

    update

--- src/App.js
+++ src/App.js
@@ -4,16 +4,16 @@ import About from "./components/About";
 import Header from "./components/Header";
 import Menu from "./components/Menu";
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import { useTranslation } from 'react-i18next';
+//import { useTranslation } from 'react-i18next';
 
 function App() {
-  const { t, i18n } = useTranslation();
+  //const { t, i18n } = useTranslation();
 
   return (
     <Router>
       <div className="App">
         <Routes>
-          <Route path="/" element={ <> <Header />  <MyContainer /> </> } />
+          <Route path="/" element={ <> <Header /> <MyContainer /> </> } />
           <Route path="/about" element={ <> <Header /> <About /> </> } />
         </Routes>
       </div>
