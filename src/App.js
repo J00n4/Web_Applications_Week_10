9c740ffc0a0f02e8847f9d2be93b93c63c972296
J00n4 <joona.ylijoki@gmail.com>
2021-11-21 19:34:59 +0200

    week 10 first push

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import './App.css';
 import MyContainer from "./components/MyContainer";
+import About from "./components/About";
+import Header from "./components/Header";
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 function App() {
 
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
-      <MyContainer />
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={ <> <Header /> <MyContainer /> </> } />
+          <Route path="/about" element={ <> <Header /> <About /> </> } />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+function Header() {
+    return(
+        <div>
+            Header text
+        </div>
+    )
+}
+
+export default Header;
